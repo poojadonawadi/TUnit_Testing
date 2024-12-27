60813e23b5b3e7aa5530c8ba2310168c13294ca6
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-27 16:22:56 +0530

    [Pooja] added: All method calls

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_Testing_practice
+{
+    public class Program
+    {
+        static void Main()
+        {
+            MSTestingDemo demo = new MSTestingDemo();
+            demo.PosOrNeg(3);
+            demo.SumOfNaturalNum(5);
+            demo.SumOfRange(2, 5);
+            demo.GetVowelCount("DotNet");
+        }
+    }
+}
